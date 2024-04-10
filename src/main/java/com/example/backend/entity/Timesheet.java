743b198ef8c9a78813a349832ba51a4c90bd23f9
ruoan1314 <lekaaniantonela@gmail.com>
2024-04-10 21:35:13 +0200

    first commit

---
+++ src/main/java/com/example/backend/entity/Timesheet.java
@@ -0,0 +1,12 @@
+package com.example.backend.entity;
+
+import com.example.backend.enums.TimeSheetStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+public class Timesheet {
+}

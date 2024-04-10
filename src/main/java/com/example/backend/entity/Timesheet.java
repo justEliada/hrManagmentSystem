59b63661505a12a57fd9cd1f8379f0f00e0bf3ff
ruoan1314 <lekaaniantonela@gmail.com>
2024-04-10 22:14:52 +0200

    first commit 1.0

--- src/main/java/com/example/backend/entity/Timesheet.java
+++ src/main/java/com/example/backend/entity/Timesheet.java
@@ -8,5 +8,30 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
-public class Timesheet {
+@Entity
+@Table(name = "timeSheets")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Timesheet extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "timeSheets"})
+    private User user;
+
+    @NotNull(message = "Starting date is required!")
+    private LocalDate fromDate;
+
+    @NotNull(message = "The end date is required!")
+    private LocalDate toDate;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Status is required")
+    private TimeSheetStatus status;
+
+    private String notes;
+
 }

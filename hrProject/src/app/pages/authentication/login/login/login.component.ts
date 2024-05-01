21e87452cb839b33128bd422e717dacad2da3468
imeldaaaaa <imeldavajushii@gmail.com>
2024-05-01 11:17:45 +0200

    user dashboard view updated

--- hrProject/src/app/pages/authentication/login/login/login.component.ts
+++ hrProject/src/app/pages/authentication/login/login/login.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent {
-
-}

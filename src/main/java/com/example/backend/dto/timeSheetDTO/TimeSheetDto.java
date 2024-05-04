1d8bd63b038037fbe2561134b9bb8ba08c259f4a
ruoan1314 <lekaaniantonela@gmail.com>
2024-05-04 16:43:20 +0200

    timesheet Dto rebuilt

--- src/main/java/com/example/backend/dto/timeSheetDTO/TimeSheetDto.java
+++ src/main/java/com/example/backend/dto/timeSheetDTO/TimeSheetDto.java
@@ -1,2 +1,21 @@
-package com.example.backend.dto.timeSheetDTO;public class TimeSheetDto {
+package com.example.backend.dto.timeSheetDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+import com.example.backend.enums.TimeSheetStatus;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TimeSheetDto {
+    private Long id;
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private TimeSheetStatus status;
+    private String notes;
+    private String createdBy;
 }

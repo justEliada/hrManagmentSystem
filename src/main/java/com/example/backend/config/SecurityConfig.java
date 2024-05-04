4a5b8b55a6344ab70c7da6c4eb6570b240ccf854
ruoan1314 <lekaaniantonela@gmail.com>
2024-05-04 14:28:47 +0200

    Security class

---
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+public class SecurityConfig {
+
+}

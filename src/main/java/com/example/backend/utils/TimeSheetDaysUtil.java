9a151c51155be48a3028b9b7d615029dea007edd
ruoan1314 <lekaaniantonela@gmail.com>
2024-05-04 17:15:51 +0200

    timesheet util

---
+++ src/main/java/com/example/backend/utils/TimeSheetDaysUtil.java
@@ -0,0 +1,9 @@
+package com.example.backend.utils;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class TimeSheetDaysUtil {
+    private TimeSheetDaysUtil() {
+    }
+}

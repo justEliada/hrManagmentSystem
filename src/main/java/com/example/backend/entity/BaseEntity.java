4f1a61d580aeab46e7bcdd0873d74d2b6046b89e
ruoan1314 <lekaaniantonela@gmail.com>
2024-05-04 14:16:48 +0200

    1.1

---
+++ src/main/java/com/example/backend/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @PastOrPresent(message = "Creation time must be in the past or present")
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String createdBy = "system";
+
+    @PastOrPresent(message = "Modification time must be in the past or present")
+    @Column(nullable = true)
+    private LocalDateTime modifiedAt;
+
+}
